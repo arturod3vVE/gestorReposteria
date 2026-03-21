c07d2e822f6f17885b15e75d7edf389160281614
cacevedo <arturod3v@gmail.com>
2026-03-21 12:34:47 -0400

    urls

--- core/urls.py
+++ core/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     
     path('p/<int:pk>/', views.public_payment_link, name='public_payment_link'),
     path('p/c/<int:customer_id>/', views.customer_bulk_payment, name='customer_bulk_payment'),
+    path('payments/<int:payment_id>/resend-telegram/', views.resend_telegram_receipt, name='resend_telegram_receipt'),
 
     path('customers/create/', views.create_customer, name='create_customer'),
     path('customers/', views.customer_list, name='customer_list'),
